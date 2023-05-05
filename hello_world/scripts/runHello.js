777e099cb45153e34d59c8854962e66f2705e586
Andrea Giuliani <andrea.giuliani12@gmail.com>
2023-05-05 22:58:25 +0200

    Changes to be committed: 	new file:   hello_world/build/contracts/HelloWorld.json 	modified:   hello_world/contracts/HelloWorld.sol 	renamed:    hello_world/migrations/1_deploy_contract.js -> hello_world/migrations/1_deploy_contracts.js 	new file:   hello_world/scripts/runHello.js

---
+++ hello_world/scripts/runHello.js
@@ -0,0 +1,14 @@
+var HelloWorld = artifacts.require("Helloworld");
+
+module.exports = async function(callback) {
+    try {
+        let hello_deployed = await HelloWorld.deployed();
+        let greet= process.argv[4];
+        const message = await hello_deployed.sayHello(greet);
+        console.log(message);
+    }
+    catch(error){
+        console.log("ERROR: "+ error);
+    }
+    callback();
+}
